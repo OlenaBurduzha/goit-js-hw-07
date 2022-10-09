dfa30a2cdb8880d15545cd0ba0db497b48736470
OlenaBurduzha <maglerb86@gmail.com>
2022-10-09 17:56:55 +0300

    Додано ДЗ

---
+++ js/01-gallery.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const imgMarkup = createImgMarkup();
+
+gallery.insertAdjacentHTML('beforeend', imgMarkup);
+gallery.addEventListener("click", onGalleryClick);
+
+
+function createImgMarkup() {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+ }).join('');
+};
+
+function onGalleryClick(event) {
+    event.preventDefault(); 
+      if (!event.target.classList.contains('gallery__image')) {
+        return
+    };
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">`)
+
+instance.show()
+};
